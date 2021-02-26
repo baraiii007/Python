465dd56fa80faa46fcf9cc38780f12fad0b669e2
baraiii007 <baraiii007@gmail.com>
2021-02-27 00:03:27 +0300

    Добавил часы и дни для первого задания

--- Zolotov_Vlad_dz_1/lesson_1_1.py
+++ Zolotov_Vlad_dz_1/lesson_1_1.py
@@ -1,5 +1,23 @@
 a = input('Введите значение:', )
 
-sec = float(a) % 60
-min = float(a) // 60
-print(a, ' это', min, 'мин', sec, 'сек')
+sec = int(a) % 60
+min = int(a) // 60
+hour = int()
+day = int()
+
+
+while min // 60:
+    if min >= 60:
+        hour = hour + min // 60
+        min = min % 60
+    elif min < 60:
+        min = min % 60
+
+while hour // 24:
+    if hour >= 24:
+        day = day + hour // 24
+        hour = hour % 24
+    elif hour < 24:
+        hour = hour % 24
+
+print(a, ' это', day, 'дней', hour, 'час', min, 'мин', sec, 'сек')
